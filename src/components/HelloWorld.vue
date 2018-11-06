ddfd1b9522020e4943f0b24ff2273c66e5824b81
chendan <603446367@qq.com>
2018-11-06 09:37:17 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="example">
+    <van-button type="primary"  plain @click="takePhoto">测试拍照</van-button>
+    <van-button type="primary"  plain @click="getGPS">测试GPS</van-button>
+    <van-button @click="show">show</van-button>
+    <van-button @click="hide">hide</van-button>
+  </div>
+</template>
+
+<script>
+import { uploadImageFromCamera} from "@/common/js/ding";
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+     
+    }
+  },
+  methods:{
+    takePhoto(){
+      let watermark={
+        time: "08:35",
+        dateWeather: "2016.05.06 周六·晴转多云 16℃",
+        username: "王晓",
+        address: "西湖·杭州"
+      };
+      uploadImageFromCamera(watermark).then(res=>{
+        console.log(res);
+      }).catch(err=>{
+        console.log(err);
+      })
+    },
+    getGPS(){
+      console.log('获取GPS');
+    },
+    show(){
+      this.Utils.loading(this,true,'请求中');
+    },
+    hide(){
+      this.Utils.loading(this,false);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
